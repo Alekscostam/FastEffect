f963256bd26e68f14d9bbdbc4e6a67786b64ef90
alekscostam <kowalski121.al@gmail.com>
2019-04-29 16:38:04 +0200

    Polaczenie z firebase

--- app/src/main/java/com/example/aleksander/fasteffect/LoginActivity.java
+++ app/src/main/java/com/example/aleksander/fasteffect/LoginActivity.java
@@ -1,19 +1,34 @@
 package com.example.aleksander.fasteffect;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.aleksander.fasteffect.FragmentClass.HouseFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView autoCompleteTextViewEmail;
+    private AutoCompleteTextView autoCompleteTextViewPassword;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +37,14 @@ public class LoginActivity extends AppCompatActivity {
         final int[] password_show = {0};
 
 
-        AutoCompleteTextView autoCompleteTextViewLogin = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewLogin);
+        autoCompleteTextViewEmail = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewEmail);
         Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
-        final AutoCompleteTextView autoCompleteTextViewPassword = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewPassword);
+        autoCompleteTextViewPassword = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextViewPassword);
         ImageButton imageButtonShowPassword = (ImageButton) findViewById(R.id.imageButtonPassword);
         TextView textViewRegister = (TextView) findViewById(R.id.textViewRegister);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         textViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,4 +85,40 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+
+    public void buttonLogin_Click(View v) {
+        String sEmail = autoCompleteTextViewEmail.getText().toString();
+        String sPassword = autoCompleteTextViewPassword.getText().toString();
+
+        if (!sEmail.matches("") && !sPassword.matches("")) {
+
+
+            final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, "Proszę czekać...", "Uwierzytelnianie", true);
+            (firebaseAuth.signInWithEmailAndPassword(autoCompleteTextViewEmail.getText().toString(), autoCompleteTextViewPassword.getText().toString()))
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(LoginActivity.this, "Zalogowano", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+
+                                i.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                                startActivity(i);
+                            } else {
+                                Log.e("Error", task.getException().toString());
+                                Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+
+        } else {
+            Toast.makeText(this, "Niekompletne dane!", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
+
+
--- app/src/main/java/com/example/aleksander/fasteffect/RegisterActivity.java
+++ app/src/main/java/com/example/aleksander/fasteffect/RegisterActivity.java
@@ -1,36 +1,87 @@
 package com.example.aleksander.fasteffect;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputEditText textInputLayoutLogin;
+    private TextInputEditText textInputLayoutPassword;
+    private TextInputEditText textInputLayoutPasswordAgain;
+    private TextInputEditText textInputLayoutEmail;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_main);
 
         TextView textViewBack = (TextView) findViewById(R.id.textViewBack);
-        TextInputLayout textInputLayoutLogin= (TextInputLayout) findViewById(R.id.textInputLayoutLogin);
-        TextInputLayout textInputLayoutPassword= (TextInputLayout) findViewById(R.id.textInputLayoutPassword);
-        TextInputLayout textInputLayoutPasswordAgain= (TextInputLayout) findViewById(R.id.textInputLayoutPasswordAgain);
-        TextInputLayout textInputLayoutEmail= (TextInputLayout) findViewById(R.id.textInputLayoutEmail);
+         textInputLayoutLogin = (TextInputEditText) findViewById(R.id.textInputEditTextLogin);
+         textInputLayoutPassword = (TextInputEditText) findViewById(R.id.textInputEditTextPassword);
+         textInputLayoutPasswordAgain = (TextInputEditText) findViewById(R.id.textInputEditTextPasswordAgain);
+         textInputLayoutEmail = (TextInputEditText) findViewById(R.id.textInputEditTextEmail);
         Button buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
 
         textViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cofnij = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent cofnij = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(cofnij);
             }
         });
 
 
     }
+
+
+
+
+    public void buttonRegister_Click(View v) {
+
+        final ProgressDialog progressDialog = ProgressDialog.show(RegisterActivity.this,"Proszę czekać...", "Rejestrowanie",true);
+        (firebaseAuth.createUserWithEmailAndPassword(textInputLayoutEmail.getText().toString(),textInputLayoutPassword.getText().toString()))
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(RegisterActivity.this, "Zarejestrowano", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(RegisterActivity.this,LoginActivity.class);
+                            startActivity(i);
+                        }
+                        else
+                        {
+                            Log.e("Error", task.getException().toString());
+                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+
+
+
 }
+
